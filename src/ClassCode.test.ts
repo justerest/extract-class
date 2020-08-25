5cb2fce97bb0a338f9d7c2f95534da7f89734531
justerest <justerest@gmailcom>
2020-08-25 17:22:55 +0500

    feat: delete unused fields, move dependencies

--- src/ClassCode.test.ts
+++ src/ClassCode.test.ts
@@ -1,40 +1,96 @@
 import { ClassCode } from './ClassCode';
-import { formatPseudoCode as f } from './formatPseudoCode';
 import { PseudoClassNode } from './PseudoClassNode';
+import { TypescriptClassNode } from './TypescriptClassNode';
+import { formatTs } from './formatTs';
 
 describe(ClassCode.name, () => {
 	describe(ClassCode.prototype.extractClass.name, () => {
 		it('should extract new class with sended method', () => {
 			const source = `
-				AB
-					+a()
-					+b()
+				class Source{
+					a(){}
+					b(){}
+				}
 			`;
-			const expected = f(`
-				A
-					+a()
+			const expected = formatTs(`
+				class Extracted{
+					a(){}
+				}
 			`);
-			const classCode = createClassCode(source);
-			const extractedClassCode = classCode.extractClass('A', ['a']);
-			expect(extractedClassCode.serialize()).toBe(expected);
+			const classCode = new ClassCode(TypescriptClassNode.from(source));
+			const extractedClassCode = classCode.extractClass('Extracted', ['a']);
+			expect(formatTs(extractedClassCode.serialize())).toBe(expected);
 		});
 
 		it('should delegate call to new property', () => {
 			const source = `
-				Source
-					+a()
-					+b()
+				class Source{
+					a(): void {}
+					b(): void {}
+				}
 			`;
-			const expected = f(`
-				Source
-					-extracted
-					+a()
-						->extracted.a()
-					+b()
+			const expected = formatTs(`
+				class Source{
+					private extracted: Extracted = new Extracted();
+
+					a(): void {
+						return this.extracted.a();
+					}
+					
+					b(): void {}
+				}
 			`);
-			const classCode = createClassCode(source);
+			const classCode = new ClassCode(TypescriptClassNode.from(source));
 			classCode.extractClass('Extracted', ['a']);
-			expect(classCode.serialize()).toBe(expected);
+			expect(formatTs(classCode.serialize())).toBe(expected);
+		});
+
+		it('should add dependency method to new class', () => {
+			const source = `
+				class Source{
+					a(){
+						this.b();
+					}
+
+					b(){}
+				}
+			`;
+			const expected = formatTs(`
+				class Extracted{
+					a(){
+						this.b();
+					}
+					
+					b(){}
+				}
+			`);
+			const classCode = new ClassCode(TypescriptClassNode.from(source));
+			const extractedClassCode = classCode.extractClass('Extracted', ['a']);
+			expect(formatTs(extractedClassCode.serialize())).toBe(expected);
+		});
+
+		it('should remove private dependency method from source if not used', () => {
+			const source = `
+			class Source{
+				a(){
+					this.b();
+				}
+				
+				private	b(){}
+			}
+		`;
+			const expected = formatTs(`
+				class Source{
+					private	extracted: Extracted = new Extracted();
+					
+					a(){
+						return this.extracted.a();
+					}
+				}
+			`);
+			const classCode = new ClassCode(TypescriptClassNode.from(source));
+			classCode.extractClass('Extracted', ['a']);
+			expect(formatTs(classCode.serialize())).toBe(expected);
 		});
 	});
 });
--- src/ClassCode.ts
+++ src/ClassCode.ts
@@ -12,31 +12,45 @@ export interface IClassNode extends ICode {
 
 export interface IInstanceMemberCode extends ICode {
 	name: string;
+	isPrivate: boolean;
+	getDependencyNames(): string[];
 	delegateTo(field: IInstanceMemberCode): void;
 	remove(): void;
 }
 
 export class ClassCode {
-	constructor(private classNode: IClassNode) {}
+	constructor(private node: IClassNode) {}
 
 	extractClass(className: string, fieldNames: string[]): ClassCode {
-		const extractedClassNode = this.classNode.clone(className);
+		const extractedClassNode = this.node.clone(className);
 		const extractedClassCode = new ClassCode(extractedClassNode);
 		extractedClassCode.removeFieldsOmit(fieldNames);
-		const createdField = this.classNode.initPrivatePropertyFor(extractedClassNode);
+		const createdField = this.node.initPrivatePropertyFor(extractedClassNode);
 		fieldNames
-			.map((fieldName) => this.classNode.getInstanceMember(fieldName))
+			.map((fieldName) => this.node.getInstanceMember(fieldName))
 			.forEach((field) => field.delegateTo(createdField));
+		this.deleteUnusedFields();
 		return extractedClassCode;
 	}
 
 	private removeFieldsOmit(fieldNames: string[]): void {
-		const fields = this.classNode.getAllInstanceMembers();
-		const excludedFields = fields.filter((field) => !fieldNames.includes(field.name));
+		const allFields = this.node.getAllInstanceMembers();
+		const dependencyFieldNames = allFields.flatMap((field) => field.getDependencyNames());
+		const usedFieldNames = new Set([...fieldNames, ...dependencyFieldNames]);
+		const excludedFields = allFields.filter((field) => !usedFieldNames.has(field.name));
 		excludedFields.forEach((field) => field.remove());
 	}
 
+	private deleteUnusedFields(): void {
+		const allFields = this.node.getAllInstanceMembers();
+		const usedFieldNames = new Set(allFields.flatMap((field) => field.getDependencyNames()));
+		const fieldsToRemove = allFields
+			.filter((field) => field.isPrivate)
+			.filter((field) => !usedFieldNames.has(field.name));
+		fieldsToRemove.forEach((field) => field.remove());
+	}
+
 	serialize(): string {
-		return this.classNode.serialize();
+		return this.node.serialize();
 	}
 }
